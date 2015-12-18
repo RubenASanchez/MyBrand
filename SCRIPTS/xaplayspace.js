732251fc799cf38623a1de9e1b52764365c04369
Valento <ruasanch@ucsc.edu>
2015-12-18 20:44:30 +0000

    updated tasks

--- SCRIPTS/xaplayspace.js
+++ SCRIPTS/xaplayspace.js
@@ -4,6 +4,23 @@ function dragTask(task,event) {
 
 function dropTask(target,event) {
     var task = event.dataTransfer.getData('task');
-    
     target.appendChild(document.getElementById(task));
 }
+
+
+/* Turns out I did not need this, used CSS haha, but I am keeping it just in case I want some further functionality */
+/*window.onload = init;
+
+function init() {
+    document.getElementById('a').onclick = addNewClass;
+    document.getElementById('b').onclick = addNewClass;
+    document.getElementById('c').onclick = addNewClass;
+    document.getElementById('d').onclick = addNewClass;
+}
+
+function addNewClass() {
+    var classString = this.className;
+    var newClass = classString.concat(" movedCompleted");
+    this.className = newClass;
+}
+*/
